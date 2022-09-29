ecdf415a21032b8997be178eed9ba906fcd0b65d
Fazle Bari <fazlebarisn@hotmail.com>
2022-09-29 22:35:56 +0600

    install composer

--- product-table-for-group-products.php
+++ product-table-for-group-products.php
@@ -1,7 +1,7 @@
 <?php
 /**
-* Plugin Name: AAA Learn WooCommerce hooks
-* Plugin URI: https://github.com/fazlebarisn/woo-product-faq
+* Plugin Name: Product table for group products
+* Plugin URI: https://github.com/fazlebarisn/product-table-for-group-products
 * Description: Learn WooCommerce hooks
 * Version: 1.0.0
 * Author: Fazle Bari
@@ -11,41 +11,47 @@
 * WC requires at least: 3.0.0
 * WC tested up to: 	 6.8.2
 * Licence: GPL Or leater
-* Text Domain: fbshooks
+* Text Domain: product-table-for-group-products
 * Domain Path: /languages/
-* @package fbshooks
+* @package grouptable
 */
 
-add_action('woocommerce_before_add_to_cart_form', 'woocommerce_before_add_to_cart_form_callback');
+defined('ABSPATH') or die('Nice Try!');
 
-function woocommerce_before_add_to_cart_form_callback(){
-    global $product;
-
-    // $product_id = 8; // ID of parent product
-
-    // wpt_query_args
-    // wpt_load
-    $grouped       = $product->is_type( 'grouped' );
-
-    add_filter('wpt_query_args' , 'wpt_query_args_callback');
-
-    function wpt_query_args_callback($args){
-        // echo "<pre>";
-        // var_dump($args);
-        // echo "</pre>";
-        global $product;
-        $product_id = $product->get_id();
-        $product    = wc_get_product( $product_id );
-        $children   = $product->get_children();
-        
-        // var_dump($children);
-
-        $args['post__in'] = $children;
-        return $args;
-    }
-
-    if ($grouped){
-        echo do_shortcode("[Product_Table id='60' name='one']");
-    }
+if( file_exists( dirname(__FILE__) . '/vendor/autoload.php') ){
+    require_once dirname(__FILE__) . '/vendor/autoload.php';
 }
+
+// add_action('woocommerce_before_add_to_cart_form', 'woocommerce_before_add_to_cart_form_callback');
+
+// function woocommerce_before_add_to_cart_form_callback(){
+//     global $product;
+
+//     // $product_id = 8; // ID of parent product
+
+//     // wpt_query_args
+//     // wpt_load
+//     $grouped       = $product->is_type( 'grouped' );
+
+//     add_filter('wpt_query_args' , 'wpt_query_args_callback');
+
+//     function wpt_query_args_callback($args){
+//         // echo "<pre>";
+//         // var_dump($args);
+//         // echo "</pre>";
+//         global $product;
+//         $product_id = $product->get_id();
+//         $product    = wc_get_product( $product_id );
+//         $children   = $product->get_children();
+        
+//         // var_dump($children);
+
+//         $args['post__in'] = $children;
+//         return $args;
+//     }
+
+//     if ($grouped){
+//         echo do_shortcode("[Product_Table id='60' name='one']");
+//     }
+// }
 
