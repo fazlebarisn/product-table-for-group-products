75fba138a4f2c78ac58dda6c25f0c0011d203fef
Fazle Bari <fazlebarisn@hotmail.com>
2022-09-29 22:55:49 +0600

    main class added

--- product-table-for-group-products.php
+++ product-table-for-group-products.php
@@ -22,6 +22,109 @@ if( file_exists( dirname(__FILE__) . '/vendor/autoload.php') ){
     require_once dirname(__FILE__) . '/vendor/autoload.php';
 }
 
+
+/**
+ * The main class
+ */
+
+final class GroupProductTable{
+
+    /**
+     * defien plugin version
+     */
+    const version = "1.0.0";
+
+    /**
+     * class constructor
+     */
+    private function __construct()
+    {
+        $this->defineConstants();
+
+        register_activation_hook( __FILE__ , [ $this , 'activate'] );
+
+        add_action( 'plugins_loaded' , [ $this , 'initPlugin'] );
+    }
+
+    /**
+     * initilize a singileton 
+     *
+     * @return \GroupProductTable class
+     */
+     public static function init(){
+
+         static $instance = false;
+
+         if( !$instance ){
+             $instance = new self();
+         }
+
+         return $instance;
+     }
+
+     /**
+      * Define plugin constants
+      *
+      * @return constants
+      */
+     public function defineConstants(){
+
+         define( 'GROUP_PTABLE_VERSION' , self::version );
+         define( 'GROUP_PTABLE_FILE' , __FILE__ );
+         define( 'GROUP_PTABLE_PATH' , __DIR__ );
+         define( 'GROUP_PTABLE_URL' , plugins_url( '' , GROUP_PTABLE_FILE ) );
+         define( 'GROUP_PTABLE_ASSETS' , GROUP_PTABLE_URL . '/assets' );
+         define( 'GROUP_PTABLE_BASENAME' , plugin_basename( __FILE__ ) );
+
+     }
+
+     /**
+      * Initialize the plugin
+      *
+      * @return void
+      */
+     public function initPlugin(){
+
+        if( is_admin() ){
+            new \Group\Ptable\Admin();
+        }else{
+            new \Group\Ptable\Frontend();
+        }
+        
+     }
+
+     /**
+      * do stuff when plugin install
+      *
+      * @return void
+      */
+     public function activate(){
+
+        // when first install
+        $installed = get_option( 'group_ptable_installed' );
+        if( !$installed ){
+            update_option( 'group_ptable_installed' , time() );
+        }
+
+        // what is the version number when first install
+        update_option( 'group_ptable_version' , GROUP_PTABLE_VERSION ); 
+
+     }
+
+ }
+
+ /**
+  * Initializes the main plugin
+  *
+  * @return \GroupProductTable class
+  */
+ function roupProductTable(){
+     return GroupProductTable::init();
+ }
+
+ // kick-off the plugin
+ roupProductTable();
+
 // add_action('woocommerce_before_add_to_cart_form', 'woocommerce_before_add_to_cart_form_callback');
 
 // function woocommerce_before_add_to_cart_form_callback(){
