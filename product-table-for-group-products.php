a132abc10ada1dd17f1ef10a66aaff620f098d55
Fazle Bari <fazlebarisn@gmail.com>
2023-03-21 12:46:43 +0600

    update version and css

--- product-table-for-group-products.php
+++ product-table-for-group-products.php
@@ -1,24 +1,25 @@
 <?php
+
 /**
-* Plugin Name: Product Table For Group Products
-* Plugin URI: https://github.com/fazlebarisn/product-table-for-group-products
-* Description: An addon plugin of product table for WooCommerce. You can display a product table on woocommerce group product. 
-* Version: 1.0.1
-* Author: Fazle Bari
-* Author URI: https://www.chitabd.com/
-* Requires PHP:             7.2
-* Tested up to:             6.1.1
-* WC requires at least:     3.0.0
-* WC tested up to: 	        7.4.0
-* Licence: GPL Or leater
-* Text Domain: product-table-for-group-products
-* Domain Path: /languages/
-* @package grouptable
-*/
+ * Plugin Name: Product Table For Group Products
+ * Plugin URI: https://github.com/fazlebarisn/product-table-for-group-products
+ * Description: An addon plugin of product table for WooCommerce. You can display a product table on woocommerce group product. 
+ * Version: 1.0.2
+ * Author: Fazle Bari
+ * Author URI: https://www.chitabd.com/
+ * Requires PHP:             7.2
+ * Tested up to:             6.1.1
+ * WC requires at least:     3.0.0
+ * WC tested up to: 	        7.5.0
+ * Licence: GPL Or leater
+ * Text Domain: product-table-for-group-products
+ * Domain Path: /languages/
+ * @package grouptable
+ */
 
 defined('ABSPATH') or die('Nice Try!');
 
-if( file_exists( dirname(__FILE__) . '/vendor/autoload.php') ){
+if (file_exists(dirname(__FILE__) . '/vendor/autoload.php')) {
     require_once dirname(__FILE__) . '/vendor/autoload.php';
 }
 
@@ -26,10 +27,11 @@ if( file_exists( dirname(__FILE__) . '/vendor/autoload.php') ){
  *  Only for developer
  * @author Fazle Bari
  */
-if( !function_exists('dd') ){
-    function dd( $val ){
+if (!function_exists('dd')) {
+    function dd($val)
+    {
         echo '<pre>';
-            var_dump($val);
+        var_dump($val);
         echo '</pre>';
     }
 }
@@ -38,7 +40,8 @@ if( !function_exists('dd') ){
  * The main class
  */
 
-final class GroupProductTable{
+final class GroupProductTable
+{
 
     /**
      * defien plugin version
@@ -52,9 +55,9 @@ final class GroupProductTable{
     {
         $this->defineConstants();
 
-        register_activation_hook( __FILE__ , [ $this , 'activate'] );
+        register_activation_hook(__FILE__, [$this, 'activate']);
 
-        add_action( 'plugins_loaded' , [ $this , 'initPlugin'] );
+        add_action('plugins_loaded', [$this, 'initPlugin']);
     }
 
     /**
@@ -62,77 +65,77 @@ final class GroupProductTable{
      *
      * @return \GroupProductTable class
      */
-     public static function init(){
+    public static function init()
+    {
 
-         static $instance = false;
+        static $instance = false;
 
-         if( !$instance ){
-             $instance = new self();
-         }
+        if (!$instance) {
+            $instance = new self();
+        }
 
-         return $instance;
-     }
+        return $instance;
+    }
 
-     /**
-      * Define plugin constants
-      *
-      * @return constants
-      */
-     public function defineConstants(){
+    /**
+     * Define plugin constants
+     *
+     * @return constants
+     */
+    public function defineConstants()
+    {
 
-         define( 'GROUP_PTABLE_VERSION' , self::version );
-         define( 'GROUP_PTABLE_FILE' , __FILE__ );
-         define( 'GROUP_PTABLE_PATH' , __DIR__ );
-         define( 'GROUP_PTABLE_URL' , plugins_url( '' , GROUP_PTABLE_FILE ) );
-         define( 'GROUP_PTABLE_ASSETS' , GROUP_PTABLE_URL . '/assets' );
-         define( 'GROUP_PTABLE_BASENAME' , plugin_basename( __FILE__ ) );
+        define('GROUP_PTABLE_VERSION', self::version);
+        define('GROUP_PTABLE_FILE', __FILE__);
+        define('GROUP_PTABLE_PATH', __DIR__);
+        define('GROUP_PTABLE_URL', plugins_url('', GROUP_PTABLE_FILE));
+        define('GROUP_PTABLE_ASSETS', GROUP_PTABLE_URL . '/assets');
+        define('GROUP_PTABLE_BASENAME', plugin_basename(__FILE__));
+    }
 
-     }
+    /**
+     * Initialize the plugin
+     *
+     * @return void
+     */
+    public function initPlugin()
+    {
 
-     /**
-      * Initialize the plugin
-      *
-      * @return void
-      */
-     public function initPlugin(){
-
-        if( is_admin() ){
+        if (is_admin()) {
             new \Group\Ptable\Admin();
-        }else{
+        } else {
             new \Group\Ptable\Frontend();
         }
-        
-     }
+    }
 
-     /**
-      * do stuff when plugin install
-      *
-      * @return void
-      */
-     public function activate(){
+    /**
+     * do stuff when plugin install
+     *
+     * @return void
+     */
+    public function activate()
+    {
 
         // when first install
-        $installed = get_option( 'group_ptable_installed' );
-        if( !$installed ){
-            update_option( 'group_ptable_installed' , time() );
+        $installed = get_option('group_ptable_installed');
+        if (!$installed) {
+            update_option('group_ptable_installed', time());
         }
 
         // what is the version number when first install
-        update_option( 'group_ptable_version' , GROUP_PTABLE_VERSION ); 
+        update_option('group_ptable_version', GROUP_PTABLE_VERSION);
+    }
+}
 
-     }
+/**
+ * Initializes the main plugin
+ *
+ * @return \GroupProductTable class
+ */
+function fbsGroupProductTable()
+{
+    return GroupProductTable::init();
+}
 
- }
-
- /**
-  * Initializes the main plugin
-  *
-  * @return \GroupProductTable class
-  */
- function fbsGroupProductTable(){
-     return GroupProductTable::init();
- }
-
- // kick-off the plugin
- fbsGroupProductTable();
-
+// kick-off the plugin
+fbsGroupProductTable();
